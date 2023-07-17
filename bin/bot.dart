f024d72dd42584955b7c52558837c13d8658e045
dhruv2981 <dhruvmanchanda040@gmail.com>
2023-07-17 19:10:32 +0530

    made a shell type where user can input

--- bin/bot.dart
+++ bin/bot.dart
@@ -1,19 +1,20 @@
 import 'dart:io';
-import 'dart:convert';
+import 'package:bot/src/models/message.dart';
 import 'package:sembast/sembast.dart';
 import 'package:bot/src/models/storage.dart';
 import 'package:bot/src/models/admin.dart';
-import 'package:bot/src/models/message.dart';
 import 'package:bot/src/models/direct_message.dart';
 import 'package:bot/src/models/server.dart';
-import 'package:args/args.dart';
-import 'package:sembast/sembast.dart';
-import 'package:sembast/sembast_io.dart';
 
-Future<void> main(List<String> arguments) async {
+
+void main() async {
   var st = Storage.constructor1();
-  // await st.connection();
-  print("why");
+  C_user c_user_c = new C_user("0", "0");
+  //the name is c_user_c because c_user is already something so to avoid confusion
+  var c_server = Server.constructor();
+  var c_channel = Channel.constructor();
+  Personal_dm c_msg = Personal_dm();
+
   List<dynamic> myList = await st.connection();
   Database db1 = myList[0];
   Database db2 = myList[1];
@@ -21,62 +22,88 @@ Future<void> main(List<String> arguments) async {
   Database db4 = myList[3];
   Database db5 = myList[4];
 
-
   StoreRef<String, String> user_store = myList[5];
   StoreRef<String, Map> server_store = myList[6];
   StoreRef<String, Map> channel_store = myList[7];
   StoreRef<Map, Map> message_store = myList[8];
   StoreRef<Map, String> p_dm_store = myList[9];
 
-
-  print("check5");
   var records = myList[10];
   var server_record = myList[11];
   var channel_record = myList[12];
   var message_record = myList[13];
   var p_dm_record = myList[14];
 
+  bool flag = true;
+  while (flag) {
+    stdout.write('\n>> ');
+    var input = stdin.readLineSync() as String;
 
-  print("check6");
-  C_user c_user_c = new C_user("0",
-      "0"); //the name is c_user_c because c_user is already something so to avoid confusion
-
-  Future<void> initial() async {
-    stdout.write('Register or Login (r/l) ');
-    final input = stdin.readLineSync();
+    // Process the input and perform desired operations
+    // final output = processInput(input);
     switch (input) {
-      case 'r':
+      case "register":
         await Admin.register(db1, user_store, records, c_user_c);
         break;
-      case 'l':
+      case "login":
         await Admin.login(db1, user_store, records, c_user_c);
         break;
+      case "join_server":
+        await c_server.join_server(db2, server_store, c_user_c, server_record);
+        break;
+      case "create_server":
+        await c_server.create_server(
+            db2, server_store, c_user_c, server_record);
+        break;
+      case "add_channel":
+        await c_channel.add_channel(db2, db3, channel_store, server_store,
+            c_user_c, channel_record, server_record);
+        break;
+      case "send_c_message":
+        await channel_message(
+            db2,
+            db3,
+            db4,
+            server_store,
+            channel_store,
+            message_store,
+            c_user_c,
+            server_record,
+            channel_record,
+            message_record);
+        break;
+      case "open_c_message":
+        await show_channel_message(
+            db2,
+            db3,
+            db4,
+            server_store,
+            channel_store,
+            message_store,
+            c_user_c,
+            server_record,
+            channel_record,
+            message_record);
+        break;
+      case "open_chats":
+        await c_msg.open_personal_dm(db5, p_dm_store, c_user_c);
+        break;
+      case "send_chat":
+        await c_msg.personal_dm(db5, db1, p_dm_store, user_store, c_user_c);
+        break;
+      // case "help":
+
+      case "logout":
+        await Admin.logout(c_user_c);
+        flag = false;
+        break;
       default:
-        print('Invalid Input');
+        print("Invalid input");
     }
   }
-
-//Asking for initial login or register
-  await initial();
-  var c_server = Server.constructor();
-  var c_channel = Channel.constructor();
-  Personal_dm c_msg = Personal_dm();
-
-
-
-  // await c_server.create_server(db2, server_store, c_user_c, server_record);
-  // await c_server.join_server(db2, server_store, c_user_c, server_record);
-  // await c_channel.add_channel(db2,db3,channel_store, server_store, c_user_c, channel_record ,server_record);
-  await channel_message(db2, db3, db4, server_store, channel_store, message_store, c_user_c, server_record, channel_record, message_record);
-  await show_channel_message(db2, db3, db4, server_store, channel_store, message_store, c_user_c, server_record, channel_record, message_record);
-  await c_msg.open_personal_dm(db5, p_dm_store, c_user_c);
-
-
-
   await db1.close();
   await db2.close();
   await db3.close();
   await db4.close();
   await db5.close();
-
 }
--- lib/src/models/storage.dart
+++ lib/src/models/storage.dart
@@ -8,20 +8,17 @@ class Storage {
   late final db4;
   late final db5;
 
-
   late final user_store;
   late final server_store;
   late final channel_store;
   late final message_store;
   late final p_dm_store;
 
-
   late final records;
   late final server_record;
   late final channel_record;
   late final message_record;
   late final p_dm_record;
-
 
   Storage.constructor1();
   Storage(
@@ -30,85 +27,62 @@ class Storage {
       this.db3,
       this.db4,
       this.db5,
-
       this.user_store,
       this.server_store,
       this.channel_store,
       this.message_store,
       this.p_dm_store,
-
       this.records,
       this.server_record,
       this.channel_record,
       this.message_record,
-      this.p_dm_record ) {
-    print("constructor called");
-  }
+      this.p_dm_record);
 
   Future<List> connection() async {
-    print("working");
     // setting up db connection
     const Path_User = 'lib/src/db/database.db';
     const Path_Server = 'lib/src/db/server.db';
-    const Path_Channel='lib/src/db/channel.db';
+    const Path_Channel = 'lib/src/db/channel.db';
     const Path_Message = 'lib/src/db/message.db';
     const Path_Personal_Dm = 'lib/src/db/personal_dm.db';
 
-
     // const dbPath1 = 'lib/src/db/c_user.db';
     final DatabaseFactory db_factory = databaseFactoryIo;
-    print("ooof");
+
     Database db1 = await db_factory.openDatabase(Path_User);
     Database db2 = await db_factory.openDatabase(Path_Server);
     Database db3 = await db_factory.openDatabase(Path_Channel);
     Database db4 = await db_factory.openDatabase(Path_Message);
     Database db5 = await db_factory.openDatabase(Path_Personal_Dm);
 
-
-    
-    print("fuck");
-
     StoreRef<String, String> user_store1 = StoreRef<String, String>.main();
-    print("check3");
     StoreRef<String, Map> server_store = StoreRef<String, Map>.main();
-    print("check4");
     StoreRef<String, Map> channel_store = StoreRef<String, Map>.main();
-    print("check5");
     StoreRef<Map, Map> message_store = StoreRef<Map, Map>.main();
-    print("check6");
-    StoreRef<Map, String> p_dm_store = StoreRef<Map,String>.main();
-
+    StoreRef<Map, String> p_dm_store = StoreRef<Map, String>.main();
 
     var records = await user_store1.find(db1);
-    var server_record =await server_store.find(db2);
-    var channel_record=await channel_store.find(db3);
-    var message_record=await message_store.find(db4);
+    var server_record = await server_store.find(db2);
+    var channel_record = await channel_store.find(db3);
+    var message_record = await message_store.find(db4);
     var p_dm_record = await p_dm_store.find(db5);
 
-
-    
-
-    // this.user_store = User;
-    print("yoyo1");
-    Storage storage =
-        Storage(
-          db1,
-          db2,
-          db3,
-          db4,
-          db5,
-          user_store1,
-          server_store,
-          channel_store,
-          message_store,
-          p_dm_store,
-          records,
-          server_record,
-          channel_record, 
-          message_record,
-          p_dm_record);
-    
-    print("yoyo");
+    Storage storage = Storage(
+        db1,
+        db2,
+        db3,
+        db4,
+        db5,
+        user_store1,
+        server_store,
+        channel_store,
+        message_store,
+        p_dm_store,
+        records,
+        server_record,
+        channel_record,
+        message_record,
+        p_dm_record);
 
     return [
       storage.db1,
@@ -123,7 +97,7 @@ class Storage {
       storage.p_dm_store,
       storage.records,
       storage.server_record,
-      storage.channel_record, 
+      storage.channel_record,
       storage.message_record,
       storage.p_dm_record
     ];
