58eefacbf28e0960e16efcf13e9918b0d57a1b44
adityachopra29 <adityachopra2912@gmail.com>
2023-07-22 19:09:52 +0530

    add category and add channel to category implemented in server.dart

--- lib/src/models/server.dart
+++ lib/src/models/server.dart
@@ -9,8 +9,8 @@ class Server {
   late String name;
   late List<Map<String, dynamic>> mem_list;
   late RoleType role;
-  late List<dynamic> cat_list;
-  late List<String> chan_list;
+  late List<Map<String, dynamic>> cat_list;
+  // late List<String> chan_list;
 
   // List<String> moderator;
 
@@ -45,6 +45,7 @@ class Server {
       List<Map> role_list = [user_role];
 
       Map<String, dynamic> s_map = {
+        'chan_list': [],
         'cat_list': [],
         'mem_list': role_list,
         's_pwd': Admin.hashPwd(s_pwd),
@@ -126,5 +127,193 @@ class Server {
       print("User successfully added to the server");
     }
     //do we need to check the user has already not joined channel
+  }
+
+  addCategory(Database db2, StoreRef<String, Map> server_store, C_user c_user1,
+      var server_record) async {
+    if (c_user1.username == "0") {
+      // print("No user has logged in you crazy fool");
+      return;
+    }
+    //some user is logged in
+    stdout.write('In which server do you want to add category: ');
+    final String s_name = stdin.readLineSync() as String;
+
+    var server_record = await server_store.find(db2);
+    for (var rec in server_record) {
+      // print(rec.key);
+      // print(rec.value);
+      // server with such name exists
+      if (rec.key == s_name) {
+        // print("Passed rec.key: " + rec.key);
+        // print(rec.value);
+
+        //means server of such name exist
+        //checking if the user is in the server
+        for (var member in rec.value['mem_list']) {
+          // print("Member: " + member);
+
+          if (member['name'] == c_user1.username &&
+              (member['role'] == 'mod' || member['role'] == 'admin')) {
+            //means the user is a mod/admin in the given server
+            stdout.write('Enter category name: ');
+            final String name = stdin.readLineSync() as String;
+
+            //catgory DNE
+            if (rec.value['cat_list'].isEmpty) {
+              //no category exists
+              //creating the category
+              // print("No category in server...hence creating category");
+              Map<String, dynamic> cat = {'name': name, 'chan_list': []};
+              // List<Map<String, dynamic>> cat_list = [];
+              // cat_list.add(cat);
+              Map<String, dynamic> pr = await server_store
+                  .record(s_name)
+                  .get(db2) as Map<String, dynamic>;
+              pr = cloneMap(
+                  pr); // Create a copy of the map //this might not work because of null error
+              pr['cat_list'].add(cat);
+              await server_store.record(s_name).delete(db2);
+              await server_store.record(s_name).put(db2, pr);
+              // print('Category created successfully 1');
+              return;
+            } else {
+              //the category list is not empty ....hence looking if the entered category is present
+              // print('Category list is not empty');
+              // print(rec.value['cat_list']);
+              // print(rec.value['cat_list']['name']);
+              for (var category in rec.value['cat_list']) {
+                if (category['name'] == name) {
+                  //category already present
+                  print('Category already exists');
+                  return;
+                }
+              }
+              //category not present in the list..but some other cateegories are present
+              // print(
+              //     "category not present in the list..but some other cateegories are present..hence adding category to list ");
+              Map<String, dynamic> cat = {'name': name, 'chan_list': []};
+              Map pr = await server_store.record(s_name).get(db2)
+                  as Map<String, dynamic>;
+              pr = cloneMap(pr);
+              pr['cat_list'].add(cat);
+              await server_store.record(s_name).delete(db2);
+              await server_store.record(s_name).put(db2, pr);
+              print('Category created successfully 2');
+              return;
+            }
+          } else if (member['name'] == c_user1.username &&
+              member['role'] == 'newbie') {
+            print('User does not have access rights');
+            return;
+          }
+        }
+        //means the user is not in the server
+        print("User is not in the server");
+        return;
+      }
+      //means such a server does not exist
+      print("Server does not exist");
+      return;
+    }
+  }
+
+  putChanInCat(
+      Database db2,
+      Database db3,
+      StoreRef<String, Map> server_store,
+      StoreRef<String, Map> channel_store,
+      C_user c_user1,
+      var server_record,
+      var channel_record) async {
+    if (c_user1.username == "0") {
+      // print("No user has logged in you crazy fool");
+      return;
+    }
+    //some user is logged in
+    stdout.write('In which server do you want to add category: ');
+    final String s_name = stdin.readLineSync() as String;
+
+    server_record = await server_store.find(db2);
+    for (var rec in server_record) {
+      // print(rec.key);
+      // print(rec.value);
+      // server with such name exists
+      if (rec.key == s_name) {
+        // print("Passed rec.key: " + rec.key);
+        // print(rec.value);
+
+        //means server of such name exist
+        //checking if the user is in the server
+        for (var member in rec.value['mem_list']) {
+          // print("Member: " + member);
+
+          if (member['name'] == c_user1.username &&
+              (member['role'] == 'mod' || member['role'] == 'admin')) {
+            //means the user is a mod/admin in the given server
+
+            stdout.write('Which channel do you want to move: ');
+            String c_name = stdin.readLineSync() as String;
+            // print(c_name);
+            // print(rec.value['chan_list']);
+            for (String channel in rec.value['chan_list']) {
+              // print(channel);
+
+              if (channel == c_name) {
+                //the indep channel exists
+                //asking which channel to put into
+                stdout.write(
+                    'In which category do you want to write the channel: ');
+                String in_cat = stdin.readLineSync() as String;
+                for (var cat in rec.value['cat_list']) {
+                  if (cat['name'] == in_cat) {
+                    //means that the category to put into also exits..putting the indep channel into the category
+                    Map<String, dynamic> po = await server_store
+                        .record(s_name)
+                        .get(db2) as Map<String, dynamic>;
+                    po = cloneMap(po);
+                    //remove the channel from chan_list
+                    po['chan_list'].remove(c_name);
+                    //add the channel in the given category's list
+                    for (var category in po['cat_list']) {
+                      if (category['name'] == in_cat) {
+                        category['chan_list'].add(c_name);
+                        await server_store.record(s_name).delete(db2);
+                        await server_store.record(s_name).put(db2, po);
+
+                        //changes in channel.db
+                        channel_record = await channel_store.find(db3);
+                        for (var rec in channel_record) {
+                          if (rec.key == c_name &&
+                              rec.value['server_name'] == s_name) {
+                            //this is the channel in the given server
+                            //changing its category from null to given category
+                            Map po = await channel_store.record(c_name).get(db3) as Map;
+                            po = cloneMap(po);
+                            po['category_name'] = in_cat;
+                            await server_store.record(c_name).delete(db3);
+                            await server_store.record(c_name).put(db3, po);
+                            print('Channel added to category successfully');
+                            return;
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+                print("Category does not exist");
+                return;
+              }
+            }
+            print('Given independant channel does not exist');
+            return;
+          }
+        }
+        print('User does not have appropriate access rights');
+        return;
+      }
+    }
+    print('Server does not exist');
+    return;
   }
 }
